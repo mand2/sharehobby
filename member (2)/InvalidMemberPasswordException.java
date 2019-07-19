5df6dfd83f85430d9ba4a43afd64ae0449acc881
son9zuen <51072273+son9zuen@users.noreply.github.com>
2019-07-19 14:11:41 +0900

    src 폴더들

---
+++ member (2)/InvalidMemberPasswordException.java
@@ -0,0 +1,10 @@
+package sharehobby.exception.member;
+
+public class InvalidMemberPasswordException extends Exception {
+
+	public InvalidMemberPasswordException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ member (3)/MemberRegService.java
@@ -0,0 +1,39 @@
+package sharehobby.service.member;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.sun.media.sound.AutoConnectSequencer;
+
+import jdbc.ConnectionProvider;
+import member.MemberInfo;
+import sharehobby.dao.member.MemberDao;
+
+public class MemberRegService {
+	private static MemberRegService service = new MemberRegService();
+	public static MemberRegService getInstance() {
+		return service;
+	}
+	
+	private MemberRegService() {};
+	
+	public int reg(MemberInfo memberInfo) {
+		int rCnt = 0;
+		
+		Connection conn = null;
+		
+		try {
+			conn = ConnectionProvider.getConnection();
+			
+			MemberDao dao = MemberDao.getInstance();
+			
+			rCnt = dao.insert(conn, memberInfo);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rCnt;
+		
+	}
+}
